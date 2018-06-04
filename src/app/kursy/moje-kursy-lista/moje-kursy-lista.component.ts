8c2ee3260608b15b009d222dac4df97f85af126d
Pendzik <andrzej.stadnik@hotmail.com>
2018-06-04 13:54:23 +0200

    account types

--- src/app/kursy/moje-kursy-lista/moje-kursy-lista.component.ts
+++ src/app/kursy/moje-kursy-lista/moje-kursy-lista.component.ts
@@ -3,6 +3,7 @@ import { Kurs } from '../shared/kurs.model';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { KursyService } from '../shared/kursy.service';
 import { KontoService } from './../../konto/shared/konto.service'
+import { User } from '../../konto/shared/user.model';
 
 
 @Component({
@@ -20,11 +21,26 @@ export class MojeKursyListaComponent implements OnInit {
   student:string 
   zapisany_test: number;
 
+  user: User[];
+  account_type: string;
+
   constructor(private db: AngularFirestore, public kursServe: KursyService,private userServe: KontoService) {
    
     this.kursServe.getKurs().subscribe(data => {
        this.kursy = data.filter(
           k => k.zapisani[k.zapisani.findIndex( z => z == this.student)] == this.student 
+        )
+        this.userServe.checkUser(this.userServe.getCurUser()).subscribe(
+          data => {
+            this.user = data
+            if (this.user.length == 0) {
+            } else {
+              this.account_type = this.user[0].account_type
+            }
+  
+  
+  
+          }
         )})
     
   }
