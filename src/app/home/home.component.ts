749db0fb992f4e1a314218dea4a785e52026dbc5
oskacz <oskiceins@gmail.com>
2018-06-04 14:42:58 +0200

    nvm

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { KontoService } from '../konto/shared/konto.service'
-
+import {DataSharingService} from '../data-sharing.service'
 
 @Component({
   selector: 'app-home',
@@ -8,8 +8,12 @@ import { KontoService } from '../konto/shared/konto.service'
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isUserLoggedIn: boolean;
+  constructor( private userServe: KontoService, public dataSharingService: DataSharingService ) {   
+    this.dataSharingService.isUserLoggedIn.subscribe( value => {
+    this.isUserLoggedIn = value;
 
-  constructor( private userServe: KontoService) { }
+    });}
 
   ngOnInit() {
     let autor=this.userServe.getCurUser()
