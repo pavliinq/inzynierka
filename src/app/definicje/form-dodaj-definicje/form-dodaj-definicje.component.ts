a5d8ca4d09e0582ec50f31320077d6054255f087
RFrankowski <r.franek93@gmail.com>
2018-06-05 12:31:40 +0200

    dodanie edytowania definicjie

--- src/app/definicje/form-dodaj-definicje/form-dodaj-definicje.component.ts
+++ src/app/definicje/form-dodaj-definicje/form-dodaj-definicje.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Definicja } from '../shared/definicja.model';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from 'angularfire2/firestore';
