ac89030473b081f65f160469d7322f5645af79ec
nasze id na git hub <pirekpaulina@gmail.com>
2018-09-18 16:17:36 +0200

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { KontoService } from '../konto/shared/konto.service'
-import {DataSharingService} from '../data-sharing.service'
+import { DataSharingService } from '../data-sharing.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
   autor:string;
   isUserLoggedIn: boolean;
   constructor( private userServe: KontoService, public dataSharingService: DataSharingService ) {   
     this.dataSharingService.isUserLoggedIn.subscribe( value => {
     this.isUserLoggedIn = value;
-
-    });
-    
+    });   
   }
 
   ngOnInit() {
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { NgForm } from '@angular/forms';
 import { KontoService } from '../konto/shared/konto.service'
 import { DataSharingService } from '../data-sharing.service'
 import { User } from '../konto/shared/user.model';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -24,32 +25,25 @@ export class NavbarComponent implements OnInit {
           } else {
             this.account_type = this.user[0].account_type
           }
-
-
-
         }
       )
-
     });
-
   }
 
   ngOnInit() {
     // let autor=this.userServe.getCurUser()
     // console.log('gasg')
     // console.log(autor)
-
   }
-
 
   dodajUsera(f: NgForm) {
     this.sloServ.setAutor(f.value.user);
 
   }
+
   wyloguj() {
     this.dataSharingService.isUserLoggedIn.next(false);
     this.userServe.setCurUser('')
-
   }
 
 }
